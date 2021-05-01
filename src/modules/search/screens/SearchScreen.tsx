85e428933224b907bf09087390cd801e13246b21
Erik Kiralj <erik.kiralj031@gmail.com>
2021-05-01 22:58:47 +0200

    Added logo and created SearchBar component

--- src/modules/search/screens/SearchScreen.tsx
+++ src/modules/search/screens/SearchScreen.tsx
@@ -1,6 +1,11 @@
 import React, { Component } from "react"
-
-export class SearchScreen extends Component {
+interface IState {
+  query: string
+}
+export class SearchScreen extends Component<{}, IState> {
+  state: IState = {
+    query: "",
+  }
   render() {
     return <div>mirko</div>
   }
