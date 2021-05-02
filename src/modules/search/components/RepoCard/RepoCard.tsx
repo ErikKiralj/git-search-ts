1ba1b2f17b3db67db3a2c30208585585a7228f7f
Erik Kiralj <erik.kiralj031@gmail.com>
2021-05-02 21:50:20 +0200

    User and Repositories model, profile and repo card

--- src/modules/search/components/RepoCard/RepoCard.tsx
+++ src/modules/search/components/RepoCard/RepoCard.tsx
@@ -1,8 +1,35 @@
 import React from "react"
+import { IRepository } from "../../../../shared/models/Repository"
 import styles from "./RepoCard.module.css"
 
-export class RepoCard extends React.Component {
+interface IProps {
+  data: IRepository[]
+}
+export class RepoCard extends React.Component<IProps, {}> {
   render() {
-    return <div className={styles.repo}></div>
+    const { data } = this.props
+    return (
+      <div className={styles.repo}>
+        {data.length !== 0 ? (
+          <>
+            {data.map((repo) => {
+              return (
+                <div key={repo.url} className={styles.card}>
+                  <h2>{repo.name}</h2>
+                  <h4>{repo.description}</h4>
+                  <a href={repo.url}>
+                    <h4>{repo.url}</h4>
+                  </a>
+                </div>
+              )
+            })}
+          </>
+        ) : (
+          <div className={styles.card}>
+            <h3>User has no repositories.</h3>
+          </div>
+        )}
+      </div>
+    )
   }
 }
